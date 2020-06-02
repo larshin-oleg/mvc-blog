d16d6261390301db217c687f36170173a1358978
larshin-oleg <56999138+larshin-oleg@users.noreply.github.com>
2020-06-02 15:14:13 +0300

    Update db.sql
    fixed db dump

--- db.sql
+++ db.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `description` text NOT NULL,
-  `text` text NOT NULL,
+  `ptext` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
